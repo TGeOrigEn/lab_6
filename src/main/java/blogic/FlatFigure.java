5c52ba00358758d6ee078f8a92a1ae0c6015aeb8
Тисло Артём <a.tislo@mcad.local>
2025-03-23 00:40:20 +0600

    Выполнен рефакторинг FlatFigure.area()

--- src/main/java/blogic/FlatFigure.java
+++ src/main/java/blogic/FlatFigure.java
@@ -40,36 +40,6 @@ public class FlatFigure {
     }
 
     public double area() {
-        if (isTriangle()) return getAreaForTriangle();
-        return getAreaForRectangle();
-    }
-
-    private double getAreaForTriangle() {
-        if (al==90) return 0.5*a*b;
-        else {
-            double p=(a+b+c)/2;
-            return Math.sqrt(p*(p-a)*(p-b)*(p-c));
-        }
-    }
-
-    private double getAreaForRectangle() {
-        if (isRectangle()) { return getAreaForSquare(); }
-        else { return getAreaForParallelogram(); }
-    }
-
-    private double getAreaForParallelogram(){
-        return a*b*Math.sin(al*Math.PI/180);
-    }
-
-    private double getAreaForSquare() {
-        return a*b;
-    }
-
-    private boolean isTriangle() {
-        return d==0;
-    }
-
-    private boolean isRectangle() {
-        return al==90 && bet==90 && a==c && b==d;
+        return FFHelp.area(this);
     }
 }
